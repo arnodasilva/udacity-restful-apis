0f653990f4b128097913559781af857f659f6f27
Arno Da Silva <arnodslv@gmail.com>
2016-12-31 17:16:42 +0100

    refactor(utils): changing json to object parsing implementation

--- json_helper.py
+++ json_helper.py
@@ -5,6 +5,5 @@ from flask import json
 
 class JSONHelper():
     @staticmethod
-    def parseFromJSONToObject(data, className):
-        object = json.loads(data, object_hook=lambda d: namedtuple(className, d.keys())(*d.values()))
-        return object
+    def parseJSONToObject(className, data):
+        return className(**json.loads(data))
