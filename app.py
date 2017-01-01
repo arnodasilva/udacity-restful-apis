68303cbd7bccebe8c1b67a3aac510308a3049b5d
Arno Da Silva <arnodslv@gmail.com>
2017-01-02 00:16:39 +0100

    feat(app): adding delete user endpoint

--- app.py
+++ app.py
@@ -46,6 +46,9 @@ def getUser(id):
     return Helper.createSuccessResponse(user.serialize, status_code=200)
 
 
+@app.route('/api/v1/users', methods=['DELETE'])
+def deleteUser():
+    return Helper.createSuccessResponse("User successfully deleted", status_code=202)
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
