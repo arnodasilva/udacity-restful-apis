aff77a36f96319b9aac55d7c6a48949777668d0c
Arno Da Silva <arnodslv@gmail.com>
2017-01-07 14:06:52 +0100

    chore(config): adding config for logging

--- config.py
+++ config.py
@@ -3,14 +3,15 @@ from os import path
 
 class Config(object):
     DEBUG = False
+
+
+class Development(Config):
+    DEBUG = True
+    SECRET_KEY = 'development'
     PORT = 5000
     HOST = '0.0.0.0'
     URL_PREFIX = '/api'
     PROJECT_ROOT = path.abspath(path.dirname(__file__))
     TEMPLATE_FOLDER = path.join(PROJECT_ROOT, 'templates')
     SQLALCHEMY_DATABASE_URI = 'sqlite:////meetneat.db'
-
-
-class Development(Config):
-    DEBUG = True
-    SECRET_KEY = 'development'
+    LOGGING_FORMAT = '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
