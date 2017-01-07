29e1ad994a9e9b6f95002720da75df965d5ef1b1
Arno Da Silva <arnodslv@gmail.com>
2017-01-07 14:07:23 +0100

    feat(app): initialization of logging

--- run.py
+++ run.py
@@ -1,7 +1,15 @@
+import logging
+from logging.handlers import RotatingFileHandler
+
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
+    handler = RotatingFileHandler('meetneat.log', maxBytes=10000, backupCount=1)
+    handler.setLevel(logging.INFO)
+    handler.setFormatter(logging.Formatter(app.config['LOGGING_FORMAT']))
+    app.logger.addHandler(handler)
+
     app.debug = True
     app.run(host=app.config['HOST'], port=app.config['PORT'])
